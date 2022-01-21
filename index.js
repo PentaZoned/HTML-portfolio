7240f3cbf0167d660617e5c3cc18c2653a720dac
Bradley Le <brabrele@gmail.com>
2022-01-21 12:32:06 -0800

    ready to be tested

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { stringify } = require('querystring');
+const { getEnabledCategories } = require('trace_events');
 
 inquirer
     .prompt([
@@ -31,4 +33,42 @@ inquirer
     ])
     .then((data) => {
         console.log(data);
+
+        var htmlString = `<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        
+        <style>
+            section {
+                background: #F5F5DC;
+                border: 4px solid black;
+                margin: 32px 250px;
+            }
+        </style>
+        
+        
+        <body>
+            <section>
+                <ul>
+                    <li>Name: ${name}</li>
+                    <li>Location: ${location}</li>
+                    <li>Bio: ${bio}</li>
+                    <li>LinkedIn URL: ${linkedin}</li>
+                    <li>Github URL: ${github}</li>
+                </ul>
+            </section>
+        
+        </body>
+        
+        </html>`;
+
+        fs.writeFile("index.txt", JSON.stringify(htmlString), (err) =>
+        err ? console.log(err) : console.log("Success!")
+        );
     })
