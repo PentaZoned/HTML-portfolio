db0fb7caa58bec8403a7d0b5fa9ffd23e8a01316
Bradley Le <brabrele@gmail.com>
2022-01-21 12:12:02 -0800

    created prompts using inquirer

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is your name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is your location?",
+            name: 'location',
+        },
+        {
+            type: 'input',
+            message: "Tell us about yourself.",
+            name: 'bio',
+        },
+        {
+            type: 'input',
+            message: "What is your LinkedIn URL?",
+            name: 'linkedin',
+        },
+        {
+            type: 'input',
+            message: "What is your Github URL?",
+            name: 'github',
+        },
+    ])
+    .then((data) => {
+        console.log(data);
+    })
